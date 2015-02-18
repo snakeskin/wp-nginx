989c025f46fb0f30200e304894d01b7c78743dc2
unknown <Aleksej_Barkhanov@EPBYMINW4780.minsk.epam.com>
2015-02-18 17:33:01 +0300

    init commit

---
+++ wp-nginx/recipes/default.rb
@@ -0,0 +1,9 @@
+require_recipe "nginx"
+
+template '/etc/nginx/sites-available/default' do
+  source 'loadbalancer.conf.erb'
+  variables({
+    :upstream_servers => node[:loadbalancer][:upstream_servers]
+  })
+  notifies :restart, resources(:service => "nginx")
+end
