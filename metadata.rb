f4304b20e62665060a364a418ffad14536de25af
unknown <Aleksej_Barkhanov@EPBYMINW4780.minsk.epam.com>
2015-02-19 09:54:27 +0300

    depends in metadata

--- metadata.rb
+++ metadata.rb
@@ -5,4 +5,4 @@ license          'All rights reserved'
 description      'Installs/Configures wp-nginx'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.1.0'
-require			 'nginx'
+depends			 'nginx'
