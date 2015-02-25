4f0cbe6e000a278999128ccf42d1ac96f5f8c490
unknown <Aleksej_Barkhanov@EPBYMINW4780.minsk.epam.com>
2015-02-25 12:12:36 +0300

    changes

--- metadata.rb
+++ metadata.rb
@@ -6,3 +6,4 @@ description      'Installs/Configures wp-nginx'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.1.0'
 depends			 'nginx'
+depends			 'nginx_vhost'
--- recipes/default.rb
+++ recipes/default.rb
@@ -1,9 +1,1 @@
-require_recipe "nginx"
-
-template '/etc/nginx/sites-available/default' do
-  source 'loadbalancer.conf.erb'
-  variables({
-    :upstream_servers => node[:loadbalancer][:upstream_servers]
-  })
-  notifies :restart, resources(:service => "nginx")
-end
+include_recipe "nginx"
